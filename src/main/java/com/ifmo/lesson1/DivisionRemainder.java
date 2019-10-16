966cae89a539db1bc8c722add8acd13cd610a4a3
Stanislav Borygin <s.borygin@mail.ru>
2019-10-16 21:15:02 +0300

    Home work 20%

---
+++ src/main/java/com/ifmo/lesson1/DivisionRemainder.java
@@ -0,0 +1,20 @@
+package com.ifmo.lesson1;
+
+public class DivisionRemainder {
+    /*
+    В переменных q и w хранятся два натуральных числа. Создайте программу,
+    выводящую на экран результат деления q на w с остатком.
+    Пример вывода программы (для случая, когда в q хранится 21, а в w хранится 8):
+    21 / 8 = 2 и 5 в остатке
+     */
+    public static void main(String[] args) {
+        int a = 25;
+        int b = 2;
+        calculate(a,b);
+    }
+    public static void calculate(int a, int b) {
+        int c = a / b;
+        int d = a % b;
+        System.out.printf("%s / %s = %s и %s в остатке\n", a, b, c, d);
+    }
+}
