ba654de3c55f095a4b3fb928b288884165d4e056
Stanislav Borygin <s.borygin@mail.ru>
2019-11-30 17:11:25 +0300

    30/11/19/17*11

--- src/main/java/com/ifmo/lesson19/injector/injector.java
+++ src/main/java/com/ifmo/lesson19/injector/injector.java
@@ -5,7 +5,7 @@ import java.lang.reflect.Field;
 import java.util.HashMap;
 import java.util.Map;
 
-class Injector {
+public class Injector {
     private Map<Class<?>, Object> singletons = new HashMap<>();
 
     @SuppressWarnings("unchecked")
